fe486d23f4eeb7f0786d19cfe421af04c9a4004a
FR0ST1N <iamfrostin@gmail.com>
2019-08-28 22:09:53 +0530

    added methods for bullet stack in player class
    jsdoc bulletStackResult typedef fix

--- src/js/player.js
+++ src/js/player.js
@@ -1,12 +1,17 @@
 /** Contains basic controls and other player functions(life, bullet stack). */
 class Player {
-  /** @param {number=} lives player's life count. */
-  constructor(lives = 1) {
+  /**
+   * @param {number} [lives=1] Player's life count.
+   * @param {number} [maxStackSize=5] Max bullets stack can hold.
+   */
+  constructor(lives = 1, maxStackSize = 5) {
     this.lives = lives;
+    this.maxStackSize = maxStackSize;
+    this.bulletStack = [];
   }
 
   /**
-   * Reduces player's life by 1.
+   * Decrement player's life by 1.
    * @return {number} Remaining life after decrement.
    */
   decrementLife() {
@@ -14,5 +19,43 @@ class Player {
       this.lives--;
     }
     return this.lives;
+  }
+
+  /**
+   * @typedef {Object} bulletStackResult
+   * @property {number|undefined} value Bullet ID.
+   * @property {boolean} success Operation result.
+   */
+
+  /**
+   * Checks size and adds bullet to the stack.
+   * @param {number|undefined} bulletId Bullet type.
+   * @return {bulletStackResult}
+   */
+  pushBullet(bulletId) {
+    let pushed = false;
+    if (this.bulletStack.length < this.maxStackSize) {
+      /*
+       * Need condidion here to not push when no bullet contact.
+       * Just perform action(animation).
+       */
+      this.bulletStack.push(bulletId);
+      pushed = true;
+    }
+    return {value: bulletId, success: pushed};
+  }
+
+  /**
+   * Removes the last added bullet from stack and returns it.
+   * @return {bulletStackResult}
+   */
+  popBullet() {
+    let popped = false;
+    let bulletId;
+    if (this.bulletStack.length > 0) {
+      bulletId = this.bulletStack.pop();
+      popped = true;
+    }
+    return {value: bulletId, success: popped};
   }
 }
