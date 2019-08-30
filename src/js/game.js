0c47f8d5a010399a842af52f73fcec134e4bc816
FR0ST1N <iamfrostin@gmail.com>
2019-08-31 01:07:07 +0530

    initial game class

---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+/** Game class where everything comes together. */
+class Game {
+  /**
+   * @param {HTMLCanvasElement} canvas Game canvas element.
+   * @param {number} width Canvas width.
+   * @param {number} height Canvas height.
+   */
+  constructor(canvas, width, height) {
+    canvas.width = width;
+    canvas.height = height;
+    this.ctx = canvas.getContext('2d');
+  }
+
+  /**
+   * Game initialization.
+   */
+  init() {
+    this.ctx.font = 'bold 48px serif';
+    this.ctx.fillText('Hello World!', 50, 100);
+  }
+}
