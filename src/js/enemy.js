5d6b50ce5b5e9457ce65815b672358ded4226d0f
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2019-09-03 02:07:06 +0530

    moved position from position.js to enemy.js, cuz build problems. added enemyImage Object for making life easier.
    todo: minifier during gulp combines files in wrong order.

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -1,3 +1,35 @@
+/**
+ * 2dimensional postion inrelation to canvas.
+ * @todo discussion and aggrement.
+ */
+class Position {
+  /**
+   * @param {number} xcordinates
+   * @param {number} ycordinates
+   */
+  constructor(xcordinates = 0, ycordinates = 0) {
+    this.x = xcordinates;
+    this.y = ycordinates;
+  }
+}
+
+/**
+ * enemyImage Object.
+ */
+class EnemyImage {
+  /**
+   * @param {string} name
+   * @return {object} image
+   */
+  constructor(name) {
+    const image= new Image();
+    image.width = '50';
+    image.height = '50';
+    image.src = 'images/'+name;
+    return image;
+  }
+}
+
 /** base class for enemy */
 class Enemy {
   /**
