4456cee2561115da3f0b4f55fbdb96be773bc12a
FR0ST1N <iamfrostin@gmail.com>
2019-11-22 21:02:40 +0530

    added license header to all files

--- src/js/font/font.js
+++ src/js/font/font.js
@@ -1,3 +1,23 @@
+/*
+ * Pew-Pew
+ * Copyright (C) 2019 Frostin
+ *
+ * This file is part of Pew-Pew.
+ *
+ * Pew-Pew is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pew-Pew is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pew-Pew.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file Contains method to draw strings in canvas.
  * @author Frostin <iamfrostin@gmail.com>
