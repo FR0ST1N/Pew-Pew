f0cacec9ea197ad49dd94e541efd09871054fe6a
FR0ST1N <iamfrostin@gmail.com>
2019-12-22 11:32:05 +0530

    added PlayerAnimator class

---
+++ src/js/player/playerAnimator.js
@@ -0,0 +1,42 @@
+/*
+ * Pew-Pew
+ * Copyright (C) 2019 Frostin
+ *
+ * This file is part of Pew-Pew.
+ *
+ * Pew-Pew is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pew-Pew is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pew-Pew.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** Absorb animation logic for player. */
+class PlayerAnimator extends Animator {
+  /** Inherited values from Animator class */
+  constructor() {
+    super();
+  }
+
+  /**
+   * Absorb animation logic
+   * States - 4 and 5
+   */
+  _absorbAnimation() {
+    if (this.frameCounter <= 15) {
+      this.animState = 4;
+    } else if (this.frameCounter >= 16 && this.frameCounter <= 30) {
+      this.animState = 5;
+    }
+    if (this.frameCounter === 30) {
+      this.frameCounter = 0;
+    }
+  }
+}
